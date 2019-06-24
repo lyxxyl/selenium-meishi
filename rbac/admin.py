36d811500ce55eb5b1abafc0edbeb87421dd6b9a
runoob <test@runoob.com>
2019-06-24 14:12:49 +0800

    zhihuuser

--- rbac/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import *
-admin.site.register(User)
-admin.site.register(Role)
-admin.site.register(Permission)
--- rbac/service/init_session.py
+++
@@ -1,7 +0,0 @@
-def set_session(request,user):
-    request.session["user_id"] = user.pk
-    permission_list = []
-    ret = user.roles.all().values("permissions__url").distinct()
-    for item in ret:
-        permission_list.append(item['permissions__url'])
-    request.session["permission"] = permission_list
